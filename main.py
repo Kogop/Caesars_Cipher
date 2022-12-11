7c382d6dbfcd058f09ba74146022c6481f5f37bf
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-12-11 22:07:06 +0300

    Все ахуенно работает

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-# import random
-
-
-# def Shifr(str):
-#   ran = random.randint(0, 33)
-#   print(ran)
-
-
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#   print("Введите предложение для зашифровки")
-#   c = input()
-#  print(c)
-#  Shifr(c)
-
-
-alfavit_RU = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+import re
+alfavit_RU = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
 itog = ''
 if __name__ == '__main__':
@@ -24,11 +9,39 @@ if __name__ == '__main__':
     for i in message_to_shipr:
         mesto = alfavit_RU.find(i)
         new_mesto = mesto + smeshenie
-        if new_mesto > 33:
-            new_mesto -=33
+        while new_mesto > 32:
+            new_mesto -= 32
         if i in alfavit_RU:
             itog += alfavit_RU[new_mesto]
         else:
             itog += i
+print("Zawivrovannoe message ->", itog)
 
-print(itog)
+check = 0
+for t in range(1, 32):
+    f = open('russian.txt', 'r')
+    first_word = next(m.group() for m in re.finditer(r'\w+', itog))
+    for line in f.readlines():
+        if re.match(first_word, line, re.I):
+            #print(line)
+            check = 1
+            break
+
+    if check == 0:
+        TempItog = '%s' % itog
+        itog = ''
+        for i in TempItog:
+            mesto = alfavit_RU.find(i)
+            new_mesto = mesto - t
+            if new_mesto < 0:
+                new_mesto += 32
+            if i in alfavit_RU:
+                itog += alfavit_RU[new_mesto]
+            else:
+                itog += i
+        print(itog)
+    else:
+        print("\n Raswifrovanniy Otwet --> ", itog)
+        break
+
+    f.close()
